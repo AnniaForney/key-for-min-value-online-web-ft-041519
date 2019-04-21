17ebd7726ef1cb284048eeacf1a03f14a0b620b7
Annia Forney <anniaforney@gmail.com>
2019-04-21 01:04:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+lowest_value = 0 
+lowest_key = nil 
 
+name_hash.index do |(x,y)|
+
+x = key 
+y = value 
 end
